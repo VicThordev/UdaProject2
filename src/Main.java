54564cf42ef9dcdc7e5fb1abda5e92349a9c3561
VicThordev <adelakinadefolahan001@gmail.com>
2021-03-22 12:17:56 +0100

    2nd Update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
+
 public class Main {
     public static void main(String[] args) {
         Simulation simulation=new Simulation();
-        Rocket rocket1 = new Rocket();
-        rocket1.land();
-        rocket1.launch();
-        for (Rocket rocket: simulation.loadU1(simulation.loadItems("C:\\Users\\User\\Documents\\Phase-1.txt")))
-        System.out.println("Here is the rocket format "+rocket);
+        simulation.runSimulation(simulation.loadU1(simulation.loadItems("C:\\Users\\User\\Documents\\Phase-1.txt")));
+        for (Rocket rocket: simulation.runSimulation(simulation.loadU1(simulation.loadItems("C:\\Users\\User\\Documents\\Phase-1.txt"))))
+            System.out.println("Here is the rocket format "+rocket);
     }
 }
